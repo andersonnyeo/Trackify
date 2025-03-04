b8f6e47acf94a1e7a6e6580a6c6895105bd7bf14
Anderson Yeo <114915975+andersonnyeo@users.noreply.github.com>
2025-03-04 18:17:24 +0000

    add in "register with email and password "

--- trackify/lib/screens/authenticate/register.dart
+++ trackify/lib/screens/authenticate/register.dart
@@ -3,7 +3,6 @@ import 'package:trackify/services/auth.dart';
 
 class Register extends StatefulWidget {
   
-
   final Function toggleView;
   const Register({super.key, required this.toggleView}); 
 
@@ -14,10 +13,13 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  
 
   // text field state
   String email = '';
-  String password = '';   
+  String password = ''; 
+  String error = '';  
 
   @override
   Widget build(BuildContext context) {
@@ -40,10 +42,12 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               const SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val!.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
@@ -51,6 +55,7 @@ class _RegisterState extends State<Register> {
               const SizedBox(height: 20.0),
               TextFormField(
                 obscureText: true,
+                validator: (val) => val!.length < 6 ? 'Enter a password 6+ chars long' : null,
                 onChanged: (val) {
                   setState(() => password = val);
                 },
@@ -66,9 +71,18 @@ class _RegisterState extends State<Register> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState!.validate()){
+                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() => error = 'please supply a valid email');
+                    }
+                  }
                 }
+              ),
+              const SizedBox(height: 12.0),
+              Text(
+                error,
+                style: const TextStyle(color: Colors.red, fontSize: 14.0),
               )
             ],
           ),
