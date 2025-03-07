c7bd8cf43b7516c7a2f88f83b25671b35931b6e9
Anderson Yeo <114915975+andersonnyeo@users.noreply.github.com>
2025-03-07 18:23:47 +0000

    make the page to slide between home and settings

--- trackify/lib/screens/wrapper.dart
+++ trackify/lib/screens/wrapper.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:trackify/screens/authenticate/authenticate.dart';
 import 'package:trackify/models/user.dart';
 import 'package:trackify/screens/home/home.dart';
+import 'package:trackify/screens/home/settings_form.dart';
 
-
-class Wrapper extends StatelessWidget {
+class Wrapper extends StatefulWidget {
   const Wrapper({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  _WrapperState createState() => _WrapperState();
+}
 
-    final User? user = Provider.of<User?>(context); // Allow nullable User
-    
+class _WrapperState extends State<Wrapper> {
+  int _selectedIndex = 0; // Ensure it starts within valid range
 
+  // List of screens (Only Home and Settings)
+  final List<Widget> _pages = [
+    Home(),
+    SettingsForm(),
+  ];
 
-    // return either Home or Authenticate widget
-    if (user == null) {
-      return const Authenticate();
-    } else {
-      return Home();
+  void _onItemTapped(int index) {
+    if (index >= 0 && index < _pages.length) { // Ensure index is valid
+      setState(() {
+        _selectedIndex = index;
+      });
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User?>(context);
+
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
+        ],
+      ),
+    );
   }
 }
